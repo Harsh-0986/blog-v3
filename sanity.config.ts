45222e8ea3b735b1767c984c100380d2562c6f2f
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-12-23 16:16:49 +0530

    Fix basic bugs

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,31 +1,33 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
-import {myTheme} from "./theme";
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+import { myTheme } from "./theme";
 import StudioNavbar from "./components/StudioNavbar";
 import Logo from "./components/Logo";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+// @ts-ignore
+const projectId: string = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+// @ts-ignore
+const dataset: string = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineConfig({
-    basePath: "/studio",
-    name: 'Blog_Studio',
-    title: 'Blog Studio',
-    projectId,
-    dataset,
+  basePath: "/studio",
+  name: "Blog_Studio",
+  title: "Blog Studio",
+  projectId,
+  dataset,
 
-    plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool()],
 
-    schema: {
-        types: schemaTypes,
+  schema: {
+    types: schemaTypes,
+  },
+  theme: myTheme,
+  studio: {
+    components: {
+      logo: Logo,
+      navbar: StudioNavbar,
     },
-    theme: myTheme,
-    studio: {
-        components: {
-            logo: Logo,
-            navbar: StudioNavbar
-        }
-    }
-})
+  },
+});
